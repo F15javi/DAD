ae524ae4f5abbe4eacfea0730e3a46f8aee5547a
F15javi <97851623+F15javi@users.noreply.github.com>
2022-05-12 18:42:12 +0200

    codigo profesor

--- Prueba Gps/src/main.cpp
+++ Prueba Gps/src/main.cpp
@@ -1,67 +1,90 @@
-#include "ArduinoJson.h"
 #include <ESP8266WiFi.h>
-#include <SoftwareSerial.h>
+#include <PubSubClient.h>
+ 
+const char* ssid = "MiFibra-E482-24G";
+const char* password = "Ng9ggSvK";
+const char* mqtt_server = "localhost";
+ 
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+ 
 
-#include <TinyGPS.h>
-
-/* This sample code demonstrates the normal use of a TinyGPS object.
-   It requires the use of SoftwareSerial, and assumes that you have a
-   4800-baud serial GPS device hooked up on pins 4(rx) and 3(tx).
-*/
-
-TinyGPS gps;
-SoftwareSerial ss(4, 3);
-
-void setup()
-{
-  Serial.begin(115200);
-  ss.begin(9600);
-  
-  Serial.print("Simple TinyGPS library v. "); Serial.println(TinyGPS::library_version());
-  Serial.println("by Mikal Hart");
+void setup_wifi() {
+ 
+  delay(10);
+  // We start by connecting to a WiFi network
   Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
-
-void loop()
-{
-  bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
-
-  // For one second we parse GPS data and report some key values
-  for (unsigned long start = millis(); millis() - start < 1000;)
-  {
-    while (ss.available())
-    {
-      char c = ss.read();
-      // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
-      if (gps.encode(c)) // Did a new valid sentence come in?
-        newData = true;
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+ 
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+ 
+}
+ 
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+      client.publish("casa/despacho/temperatura", "Enviando el primer mensaje");
+      client.subscribe("casa/despacho/luz");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
     }
   }
-
-  if (newData)
-  {
-    float flat, flon;
-    unsigned long age;
-    gps.f_get_position(&flat, &flon, &age);
-    Serial.print("LAT=");
-    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
-    Serial.print(" LON=");
-    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
-    Serial.print(" SAT=");
-    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
-    Serial.print(" PREC=");
-    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+}
+void setup() {
+  //pinMode(BUILTIN_LED, OUTPUT);
+  Serial.begin(9600);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+ 
+ 
+void loop() {
+ 
+  if (!client.connected()) {
+    reconnect();
   }
-  
-  gps.stats(&chars, &sentences, &failed);
-  Serial.print(" CHARS=");
-  Serial.print(chars);
-  Serial.print(" SENTENCES=");
-  Serial.print(sentences);
-  Serial.print(" CSUM ERR=");
-  Serial.println(failed);
-  if (chars == 0)
-    Serial.println("** No characters received from GPS: check wiring **");
+  client.loop();
+ 
+  long now = millis();
+  if (now - lastMsg > 2000) {
+    lastMsg = now;
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("casa/despacho/temperatura", msg);
+  }
 }
