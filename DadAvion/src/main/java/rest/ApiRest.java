6e88a3e7cffd787ed252647194a8e9401ee285a7
felartagu99 <102315373+felartagu99@users.noreply.github.com>
2022-05-16 12:32:01 +0200

    Añadiendo todos los get y post

--- DadAvion/src/main/java/rest/ApiRest.java
+++ DadAvion/src/main/java/rest/ApiRest.java
@@ -63,7 +63,14 @@ public class ApiRest extends AbstractVerticle{
 		
 		router.route("api/fly*").handler(BodyHandler.create());
 		router.get("/api/fly").handler(this::getAllWithConnectionFLY);
-	
+		router.get("/api/fy/:id").handler(this::getById_Fly);
+		router.post("/api/fly").handler(this::postFly);
+		
+		router.route("api/airport*").handler(BodyHandler.create());
+		router.get("/api/airport").handler(this::getAllWithConnectionAIRPORT);
+		router.get("/api/airport/:id").handler(this::getById_Airport);
+		router.post("/api/airport").handler(this::postAirport);
+	 
 		
 		
 		MqttClient mqttClient = MqttClient.create(vertx, new MqttClientOptions().setAutoKeepAlive(true).setUsername("admin").setPassword("admin"));
@@ -176,6 +183,31 @@ public class ApiRest extends AbstractVerticle{
 		});
 	}
 	
+	private void getAllWithConnectionAIRPORT(RoutingContext routingContext) {
+		mySqlClient.getConnection(connection -> {
+			if (connection.succeeded()) {
+				connection.result().query("SELECT * FROM dad_db_avion.airport;").execute( res -> {
+					if (res.succeeded()) {
+						// Get the result set
+						RowSet<Row> resultSet = res.result();
+						System.out.println(resultSet.size());
+						JsonArray result = new JsonArray();
+						for (Row elem : resultSet) {
+							result.add(JsonObject
+									.mapFrom(new Airport(elem.getInteger("id_Airport"), elem.getString("name"), 
+											 elem.getDouble("lat"), elem.getDouble("lon"))));
+						}
+						System.out.println(result.toString());
+					} else {
+						System.out.println("Error: " + res.cause().getLocalizedMessage());
+					}
+					connection.result().close();
+				});
+			} else {
+				System.out.println(connection.cause().toString());
+			}
+		});
+	}
 	
 	private void getById_Gps(RoutingContext routingContext) {
 		Integer id_Gps = Integer.parseInt(routingContext.request().getParam("id_Gps"));
@@ -206,8 +238,59 @@ public class ApiRest extends AbstractVerticle{
 	}
 	
 	
+	private void getById_Fly(RoutingContext routingContext) {
+		Integer id_Fly = Integer.parseInt(routingContext.request().getParam("id_Fly"));
+		mySqlClient.getConnection(connection -> {
+			if (connection.succeeded()) {
+				connection.result().preparedQuery("SELECT * FROM dad_db_avion.fly WHERE id_Fly = '" + id_Fly + "'").execute(
+						Tuple.of(id_Fly), res -> {
+							if (res.succeeded()) {
+								// Get the result set
+								RowSet<Row> resultSet = res.result();
+								System.out.println(resultSet.size());
+								JsonArray result = new JsonArray();
+								for (Row elem : resultSet) {
+									result.add(JsonObject.mapFrom(new Fly(elem.getInteger("id_Fly"), elem.getInteger("id_AirporDest"), 
+											elem.getInteger("id_AirporOrig"), elem.getString("plate"),  elem.getLong("time_Dep"), elem.getLong("time_Arr"))));
+								}
+								System.out.println(result.toString());
+							} else {
+								System.out.println("Error: " + res.cause().getLocalizedMessage());
+							}
+							connection.result().close();
+						});
+			} else {
+				System.out.println(connection.cause().toString());
+			}
+		});
+	}
 	
-	
+	private void getById_Airport(RoutingContext routingContext) {
+		Integer id_Airport = Integer.parseInt(routingContext.request().getParam("id_Airport"));
+		mySqlClient.getConnection(connection -> {
+			if (connection.succeeded()) {
+				connection.result().preparedQuery("SELECT * FROM dad_db_avion.airport WHERE id_Airport = '" + id_Airport + "'").execute(
+						Tuple.of(id_Airport), res -> {
+							if (res.succeeded()) {
+								// Get the result set
+								RowSet<Row> resultSet = res.result();
+								System.out.println(resultSet.size());
+								JsonArray result = new JsonArray();
+								for (Row elem : resultSet) {
+									result.add(JsonObject.mapFrom(new Airport(elem.getInteger("id_Airport"), elem.getString("name"), 
+											 elem.getDouble("lat"), elem.getDouble("lon"))));
+								}
+								System.out.println(result.toString());
+							} else {
+								System.out.println("Error: " + res.cause().getLocalizedMessage());
+							}
+							connection.result().close();
+						});
+			} else {
+				System.out.println(connection.cause().toString());
+			}
+		});
+	}
 	
 	private void postGps(RoutingContext routingContext){
 		final Gps gps = gson.fromJson(routingContext.getBodyAsString(), Gps.class);	
